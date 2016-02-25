4b668bc25862c719ef6a4e9cacff8f3660a5e3ff
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-02-25 14:38:10 +0900

    Create index.php

---
+++ DisplayName/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to show the time</title>
+</head>
+<body>
+<h1>TYPE AND DISPLAY YOUR NAME</h1>
+
+<form action="display.php" method="get">
+<label for="my_name">NAME:</label>
+<input id="my_name" type="text" name="my_name" size="35" maxlength="255" value="" />
+<input type="submit" value="send" />
+</form>
+
+</body>
+</html>
