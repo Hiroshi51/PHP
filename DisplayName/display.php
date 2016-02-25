67667a99a0e0752027b54ca70b5174efbf616579
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-02-25 14:38:35 +0900

    Create display.php

---
+++ DisplayName/display.php
@@ -0,0 +1,6 @@
+<?php 
+echo ('NAME : ' . htmlspecialchars($_REQUEST['my_name'],ENT_QUOTES));
+/*****************************************
+ * ENT_QUOTES : safely get single quotations
+ ****************************************/
+?>
