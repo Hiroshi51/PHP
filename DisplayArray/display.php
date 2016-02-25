5684be2a31a5d769e8ba231e1a83ec9270ca6d77
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-02-25 15:52:52 +0900

    Create display.php

---
+++ DisplayArray/display.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<title>How to show the time</title>
+</head>
+<body>
+<?php 
+echo ('Prefecture : ' . htmlspecialchars($_POST['pref'],ENT_QUOTES));
+?>
+</body>
+</html>
