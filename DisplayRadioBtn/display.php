b646731b52891a8936e6b3ff6af3a4fe0102cbe3
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-02-25 15:55:23 +0900

    Update display.php

--- DisplayRadioBtn/display.php
+++ DisplayRadioBtn/display.php
@@ -1,7 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<title>How to show the time</title>
+</head>
+<body>
 <?php 
 echo ('NAME : ' . htmlspecialchars($_POST['my_name'],ENT_QUOTES));
 echo ('<br>');
 echo ('GENDER : ' . htmlspecialchars($_POST['gender'],ENT_QUOTES));
+echo ('<br>');
+echo ('Age : ' . htmlspecialchars($_POST['age'],ENT_QUOTES));
+echo ('<br>');
+echo ('Prefecture : ' . htmlspecialchars($_POST['pref'],ENT_QUOTES));
+?>
+</body>
+</html>
 /*****************************************
  * ENT_QUOTES : safely get single quotations
  ****************************************/
